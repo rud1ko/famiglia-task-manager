d812c9a95e0538ea84486b9042582b99003c8970
Rudiko777 <chakryan_rudik@mail.ru>
2024-11-09 13:05:09 +0300

    first commit

---
+++ src/main/java/Task/management/project/in/famiglia/app/FamigliaTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package Task.management.project.in.famiglia.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamigliaTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamigliaTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Task/management/project/in/famiglia/app/FamigliaTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Task.management.project.in.famiglia.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamigliaTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
